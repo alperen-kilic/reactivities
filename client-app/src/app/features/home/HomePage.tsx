146c6ab5d8f3123439fdcdd57ac859aa984d2f82
Alperen Kılıç <alp.kilic.97@gmail.com>
2023-10-27 13:26:17 +0300

    removed ! from title

--- client-app/src/app/features/home/HomePage.tsx
+++ client-app/src/app/features/home/HomePage.tsx
@@ -16,9 +16,9 @@ const HomePage = () => {
         </Header>
         {userStore.isLoggedIn ? (
           <>
-            <Header as="h2" inverted content="Welcome to Reactivities!" />
+            <Header as="h2" inverted content="Welcome to Reactivities" />
             <Button as={Link} to="/activities" size="huge" inverted>
-              Go to Activities!
+              Go to Activities
             </Button>
           </>
         ) : (
