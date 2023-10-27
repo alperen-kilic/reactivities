d39bb7cd1eff3a036dc2a018f2855f141659f795
Alperen Kılıç <alp.kilic.97@gmail.com>
2023-10-27 12:25:07 +0300

    Updated buggy url

--- client-app/src/app/features/errors/TestError.tsx
+++ client-app/src/app/features/errors/TestError.tsx
@@ -4,41 +4,30 @@ import { useState } from "react";
 import ValidationError from "./ValidationError";
 
 export default function TestErrors() {
-  const baseUrl = import.meta.env.VITE_API_URL + "/";
   const [errors, setErrors] = useState(null);
 
   function handleNotFound() {
-    axios
-      .get(baseUrl + "buggy/not-found")
-      .catch((err) => console.log(err.response));
+    axios.get("/buggy/not-found").catch((err) => console.log(err.response));
   }
 
   function handleBadRequest() {
-    axios
-      .get(baseUrl + "buggy/bad-request")
-      .catch((err) => console.log(err.response));
+    axios.get("/buggy/bad-request").catch((err) => console.log(err.response));
   }
 
   function handleServerError() {
-    axios
-      .get(baseUrl + "buggy/server-error")
-      .catch((err) => console.log(err.response));
+    axios.get("/buggy/server-error").catch((err) => console.log(err.response));
   }
 
   function handleUnauthorized() {
-    axios
-      .get(baseUrl + "buggy/unauthorized")
-      .catch((err) => console.log(err.response));
+    axios.get("/buggy/unauthorized").catch((err) => console.log(err.response));
   }
 
   function handleBadGuid() {
-    axios
-      .get(baseUrl + "activities/notaguid")
-      .catch((err) => console.log(err.response));
+    axios.get("/activities/notaguid").catch((err) => console.log(err.response));
   }
 
   function handleValidationError() {
-    axios.post(baseUrl + "activities", {}).catch((err) => setErrors(err));
+    axios.post("/activities", {}).catch((err) => setErrors(err));
   }
 
   return (
@@ -47,30 +36,10 @@ export default function TestErrors() {
       <Segment>
         <Button.Group widths="7">
           <Button onClick={handleNotFound} content="Not Found" basic primary />
-          <Button
-            onClick={handleBadRequest}
-            content="Bad Request"
-            basic
-            primary
-          />
-          <Button
-            onClick={handleValidationError}
-            content="Validation Error"
-            basic
-            primary
-          />
-          <Button
-            onClick={handleServerError}
-            content="Server Error"
-            basic
-            primary
-          />
-          <Button
-            onClick={handleUnauthorized}
-            content="Unauthorized"
-            basic
-            primary
-          />
+          <Button onClick={handleBadRequest} content="Bad Request" basic primary />
+          <Button onClick={handleValidationError} content="Validation Error" basic primary />
+          <Button onClick={handleServerError} content="Server Error" basic primary />
+          <Button onClick={handleUnauthorized} content="Unauthorized" basic primary />
           <Button onClick={handleBadGuid} content="Bad Guid" basic primary />
         </Button.Group>
       </Segment>
